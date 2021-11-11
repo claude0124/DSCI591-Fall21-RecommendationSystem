dcc9f7811be9d83f522874c4186d298c80788a7d
Dayun Piao <36489733+claude0124@users.noreply.github.com>
2021-11-11 17:58:30 -0500

    Add files via upload

---
+++ eda/tentree_eda.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import seaborn as sns
+import re
+import warnings
+warnings.filterwarnings("ignore")
+
+
+df = pd.read_csv(r"/Users/Claude/Desktop/GraduateStudy/Fall21/DSCI591/web_scrapping/tentree.csv")
+
+# NOTE: image_url might not be complete
+# Select targeted attributes
+df = df[["gender", "price", "materials", "color", "image_url", "description", "name"]]
+
+# rename column names to match column names of HM
+df = df.rename(columns = {"name": "title", "materials": "composition", "image_url": "imageUrl"})
+
+# add brand column
+df["brand"] = "tentree"
+
+# rearrange columns order
+new_columns_order = ["description", "price", "color", "gender", "imageUrl", "brand", "composition"]
+df = df[new_columns_order]
+
+# extract each material out of composition column
+materials_lst = []
+for index, row in df.iterrows():
+    # pattern found in Tentree's composition columns
+    composition = row["composition"].replace("Recycled ", "").replace("TENCEL ", "").replace("Organic ", "").lower()
+    matches = re.compile(r'(\d+[%])\s(\w+)').finditer(composition)
+    for match in matches:
+        materials_lst.append(match.group(2))
+
+materials_lst = list(set(materials_lst))
+
+print(materials_lst)
+
+# create new columns based on materials list
+for material in materials_lst:
+    df[material] = ""
+
+# adding value to each new material columns based on composition column value
+for material in materials_lst:
+    data = []
+    for index, row in df.iterrows():
+        composition = row["composition"].replace("Recycled ", "").replace("TENCEL ", "").replace("Organic ", "").lower()
+        tmp = re.findall(r'(\d+)[%]\s({})'.format(material), composition)
+        if len(tmp) == 0:
+            data.append(0)
+        else:
+            data.append(int(tmp[0][0])/100)
+    df[material] = data
+
+
+# set title column as index
+df.set_index("title", inplace = True)
+
+# modify gender column value
+df['gender'] = df['gender'].map({"mens": "men", "womens": "women"})
+
+print(df.columns)  # check all the column names
+print(df.head())
+
+# save the cleaned data as csv file
+df.to_csv("./tentree_clean.csv")
---
+++ eda/zara_eda.py
@@ -0,0 +1,99 @@
+#%%
+import pandas as pd
+import seaborn as sns
+import re
+import warnings
+warnings.filterwarnings("ignore")
+
+
+df = pd.read_csv(r"/Users/Claude/Desktop/GraduateStudy/Fall21/DSCI591/web_scrapping/zara.csv", index_col = "title")
+
+# Select targeted attributes
+df = df[["gender", "price", "composition", "color", "imageUrl", "description"]]
+
+# clean price column
+def delete_USD(x):
+    x = x.replace("USD", "")
+    return(float(x))
+df["price"] = df["price"].apply(delete_USD)
+print(df["price"].head())
+print("-"*90)
+
+# clean color column
+# TODO: clean row if color value is only integer
+def extract_color(x):
+  x = x.split("|")[0]
+  if "Color" in x:
+    x = x.replace("Color", "")
+  if "/" in x:
+    x = x.replace("/ ", "")
+  return x
+df["color"] = df["color"].apply(extract_color)
+print(df["color"].head())
+print("-"*90)
+
+# Add new column of 'brand'
+df["brand"] = "Zara"
+
+# rearrange columns order
+new_columns_order = ["description", "price", "color", "gender", "imageUrl", "brand", "composition"]
+df = df[new_columns_order]
+
+# clean composition column
+def extract_composition(x):
+    material = [i for i in x.split(",") if "%" in i and ":" not in i][0]
+    return material
+df["composition"] = df["composition"].apply(extract_composition)
+
+# extract each material out of composition column
+materials_lst = []
+for index, row in df.iterrows():
+    composition = row["composition"].lower()
+    matches = re.compile(r'(\d+[%])\s(\w+)').finditer(composition)
+    for match in matches:
+        materials_lst.append(match.group(2))
+
+materials_lst = list(set(materials_lst))
+materials_lst.remove("of")  # there is only one outlier of 'of' material
+
+print(materials_lst)
+
+# create new columns based on materials list
+for material in materials_lst:
+    df[material] = ""
+
+# adding value to each new material columns based on composition column value
+for material in materials_lst:
+    data = []
+    for index, row in df.iterrows():
+        composition = row["composition"].lower()
+        tmp = re.findall(r'(\d+)[%]\s({})'.format(material), composition)
+        if len(tmp) == 0:
+            data.append(0)
+        else:
+            data.append(int(tmp[0][0])/100)
+    df[material] = data
+print("-"*90)
+
+# get statistic summary about price column
+print(df["price"].describe())
+
+print(df.head())
+print("\nData cleaned!")
+
+print("\n###### EDA ######")
+#plot_order1 = df["price"].groupby('price')['price'].sum().sort_values(ascending=True).index.values
+g = sns.catplot("price", data=df, kind='count', height=10, aspect=1.5)
+(g.set_axis_labels("Price", "Count")
+  .set_titles("Zara Price distribution")
+#   .set(ylim=(0, 1))
+  .despine(left=True))  
+
+
+# save it in the current working directory
+output_name = "zara_clean.csv"
+df.to_csv(f"./{output_name}")
+print(f"dataframe saved as {output_name} in the current path")
+print("Done\n")
+
+# %%
