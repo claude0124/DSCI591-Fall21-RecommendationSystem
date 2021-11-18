783e654be1ea45bd0fc01c53b4f7b35c115516bb
Dayun Piao <claude0124p@gmail.com>
2021-11-18 15:26:29 -0500

    Updates on missing scatter plots and more EDA visualizations

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -60,21 +60,23 @@ def plot_image_distribution(dim_df, brand):
     except:
         print(brand)
 
-    plt.figure(0)
+    # plt.figure(0)
     sizes.plot.scatter(x='width', y='height')
     plt.title('Image Sizes (pixels) | {}'.format(brand))
     plt.xlabel("width")
     plt.ylabel("height")
-    plt.show()
     plt.savefig(f"./EDA/imageSizes_scatter_{brand}.png")
+    plt.show()
 
-    plt.figure(1)
+    # plt.figure(1)
+
     sizes.plot.bar(x="h_w", y="count")
     plt.title('Image Sizes Counts (pixels) | {}'.format(brand))
     plt.xlabel("image sizes")
     plt.ylabel("count")
     for i, v in enumerate(list(sizes["count"])):
         plt.text(i, v + 3, str(v), color='blue', fontweight='bold')
+    plt.xticks(rotation=45)
     plt.savefig(f"./EDA/imageSizes_bar_{brand}.png")
     plt.show()
 
@@ -92,31 +94,35 @@ if __name__ == "__main__":
 
     df = pd.read_csv("./data/E-Weaver_data.csv", index_col = [0])
 
-    # statistical description for datatypes 
-    # print(df.info())
-    # print("-"*50)
-    # print(df.dtypes)
-    # print("-"*50)
-    # print(df.describe().T)
-    # print("-"*50)
+    # Statistical description for datatypes 
+    print(df.info())
+    print("-"*50)
+    print(df.dtypes)
+    print("-"*50)
+    print(df.describe().T)
+    print("-"*50)
 
-    # # Checking the number of rows having null values
-    # print(df[df.columns[df.isnull().any()]].isnull().sum())
-    # print("-"*50)
+    # Checking the number of rows having null values
+    print(df[df.columns[df.isnull().any()]].isnull().sum())
+    print("-"*50)
 
-    # # How many unique colors in dataset
-    # print(len(df["color"].unique()))
+    # How many unique colors in dataset
+    print(len(df["color"].unique()))
 
-    # # Price columns' distribution in bar plot
-    # df['price'].value_counts().head(10).plot.bar()
-    # plt.savefig(f"./EDA/price_histogram.png")
-    # plt.show()
+    # Price columns' distribution in bar plot
+    df['price'].value_counts().head(10).plot.bar()
+    plt.title("Price counts in descending order")
+    plt.xlabel("price($)")
+    plt.ylabel("counts")
+    plt.savefig(f"./EDA/price_histogram.png")
+    plt.show()
 
     # # Outlier analysis of price column
-    # price_df = df["price"]
-    # ax = sns.boxplot(data = price_df, palette = "Set2")
-    # plt.savefig(f"./EDA/price_outlier.png")
-    # plt.show()
+    price_df = df["price"]
+    ax = sns.boxplot(data = price_df, palette = "Set2")
+    plt.title("Outlier analysis of price attribute")
+    plt.savefig(f"./EDA/price_outlier.png")
+    plt.show()
 
     # Correlation analysis between numerical value attributes
     columns = df.columns.tolist()
@@ -133,60 +139,67 @@ if __name__ == "__main__":
     plt.savefig(f"./EDA/numerical_feature_heatmap.png")
     plt.show()
 
-    # brand_lst = list(df.brand.unique())
-    # for brand in brand_lst:
-    #     # Create wordcloud for each indivicual brand
-    #     tmp_df = df[df["brand"]==brand]
-    #     make_wordcloud(tmp_df, brand)
+    brand_lst = list(df.brand.unique())
+    for brand in brand_lst:
+        # Create wordcloud for each indivicual brand
+        tmp_df = df[df["brand"]==brand]
+        make_wordcloud(tmp_df, brand)
 
-    #     # Create visualization for images'size distribution
-    #     if brand == "tentree":
-    #         continue
-    #     imagePath_lst = [os.path.join("data/image_data", f) for f in os.listdir("data/image_data/") if brand in f]
-    #     dim_df = pd.DataFrame([get_dims(imagePath) for imagePath in imagePath_lst], columns = ["height", "width"])
-    #     dim_df = dim_df[dim_df.height != 0]
-    #     plot_image_distribution(dim_df, brand)
+        # Create visualization for images'size distribution
+        if brand == "tentree":
+            continue
+        imagePath_lst = [os.path.join("data/image_data", f) for f in os.listdir("data/image_data/") if brand in f]
+        dim_df = pd.DataFrame([get_dims(imagePath) for imagePath in imagePath_lst], columns = ["height", "width"])
+        dim_df = dim_df[dim_df.height != 0]
+        plot_image_distribution(dim_df, brand)
 
-    # # Create wordcloud for all data
-    # make_wordcloud(df, "all")
+    # Create wordcloud for all data
+    make_wordcloud(df, "all")
 
-    # # Create visualization for price column
-    # # plot_order1 = df["price"].groupby('price')['price'].sum().sort_values(ascending=True).index.values
-    # g = sns.catplot("price", data=df, kind='count', height=10, aspect=1.5)
-    # (g.set_axis_labels("Price", "Count").set_titles("All data price distribution").despine(left=True))  
+    # Create visualization for all images's size
+    imagePath_lst = [os.path.join("data/image_data", f) for f in os.listdir("data/image_data/")]
+    dim_df = pd.DataFrame([get_dims(imagePath) for imagePath in imagePath_lst], columns = ["height", "width"])
+    # Get rid of image failed to download which only has 1 channel only
+    dim_df = dim_df[dim_df.height != 0]
+    plot_image_distribution(dim_df, "all")
 
-    # # Create visualization for all the composition materials
-    # df_men = df[df["gender"]=="men"]
-    # df_women = df[df["gender"]=="women"]
+    # Create visualization for price column
+    # plot_order1 = df["price"].groupby('price')['price'].sum().sort_values(ascending=True).index.values
+    g = sns.catplot("price", data=df, kind='count', height=10, aspect=1.5)
+    (g.set_axis_labels("Price", "Count").set_titles("All data price distribution").despine(left=True))  
 
-    # plot_material_distribution(df, materials_columns, "all")
-    # # Create visualization for each gender's composition materials
-    # plot_material_distribution(df_men, materials_columns, "men")
-    # plot_material_distribution(df_women, materials_columns, "women")
-    # # Create visualization for each brand's composition materials
-    # for brand in brand_lst:
-    #     tmp_df = df[df["brand"]==brand]
-    #     plot_material_distribution(tmp_df, materials_columns, brand)
+    # Create visualization for all the composition materials
+    df_men = df[df["gender"]=="men"]
+    df_women = df[df["gender"]=="women"]
 
-    # # Create distribution of numbers of materials in a t-shirt
-    # # first get boolean value for all the numbers value in all materials sub-dataset, True for any number and False for 0
-    # df_boolean = df[materials_columns].select_dtypes(include=['number']) != 0
-    # count_lst = []
-    # for idx, row in df_boolean.iterrows():
-    #     count = 0
-    #     for material in materials_columns:
-    #         if row[material] == True:
-    #             count += 1 
-    #     count_lst.append(count)
-    # count_dict = dict(Counter(count_lst))
-    # # rename the keys
-    # count_dict = dict((str(key)+" material(s)", value) for (key, value) in count_dict.items())
-    # # print(count_dict)
+    plot_material_distribution(df, materials_columns, "all")
+    # Create visualization for each gender's composition materials
+    plot_material_distribution(df_men, materials_columns, "men")
+    plot_material_distribution(df_women, materials_columns, "women")
+    # Create visualization for each brand's composition materials
+    for brand in brand_lst:
+        tmp_df = df[df["brand"]==brand]
+        plot_material_distribution(tmp_df, materials_columns, brand)
 
-    # fig = go.Figure(data=[go.Pie(labels=list(count_dict.keys()), values = list(count_dict.values()),
-    #                                     title = f"material composition count")])
-    # fig.show()
-    # fig.write_image("./EDA/material_composition_count.png")
+    # Create distribution of numbers of materials in a t-shirt
+    # first get boolean value for all the numbers value in all materials sub-dataset, True for any number and False for 0
+    df_boolean = df[materials_columns].select_dtypes(include=['number']) != 0
+    count_lst = []
+    for idx, row in df_boolean.iterrows():
+        count = 0
+        for material in materials_columns:
+            if row[material] == True:
+                count += 1 
+        count_lst.append(count)
+    count_dict = dict(Counter(count_lst))
+    # rename the keys
+    count_dict = dict((str(key)+" material(s)", value) for (key, value) in count_dict.items())
+    # print(count_dict)
+
+    fig = go.Figure(data=[go.Pie(labels=list(count_dict.keys()), values = list(count_dict.values()),
+                                        title = f"material composition count")])
+    fig.show()
+    fig.write_image("./EDA/material_composition_count.png")
 
 
 
