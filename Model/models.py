675590fd9b31c3388116fde36ee0c77de65ae58a
Dayun Piao <36489733+claude0124@users.noreply.github.com>
2022-01-18 23:48:07 -0500

    generated features data for ml models

--- Model/models.py
+++ Model/models.py
@@ -2,6 +2,7 @@
 from numpy import integer, unique, where, argmax
 import numpy as np
 import pandas as pd
+import os
 from matplotlib import pyplot as plt
 
 from sklearn.mixture import GaussianMixture
@@ -15,7 +16,7 @@ def categorical_encoding(df, col_name):
     dataframe to numerical values to fit in ML models
     """
     # Preprocess categorical columns
-    col_np = df[[col_name]].to_numpy()
+    col_np = df[[col_name]].to_numpy().ravel()
 
     # Integer encode
     label_encoder = LabelEncoder()
@@ -31,69 +32,82 @@ def categorical_encoding(df, col_name):
     return one_hot_encoded
 
 
-# Load dataset
-df = pd.read_csv("./data/E-Weaver_data.csv", index_col = [0])
+def gen_features(df):
+    """
+    This function generate features from E-Weaver_data.csv's selected columns.
+    Columns include: color, gender, brand, price, and each materials columns.
+    """
+    # Convert categorical features into numerical values
+    for col_name in ['color', 'gender', 'brand']:
+        if 'features' not in locals():
+            features = categorical_encoding(df, col_name)
+        else:
+            tmp = categorical_encoding(df, col_name)
+            features = np.concatenate((features, tmp),axis=1) 
 
-# Convert categorical features into numerical values
-for col_name in ['color', 'gender', 'brand']:
-    if 'features' not in globals():
-        features = categorical_encoding(df, col_name)
-    else:
-        tmp = categorical_encoding(df, col_name)
-        features = np.concatenate((features, tmp),axis=1) 
+    # Get the column names for each materials
+    columns = df.columns.tolist()
+    composition_index = columns.index("composition")
+    materials_columns = columns[composition_index+1:-1]
 
-# Add the rest of numerical features of 'price' and each individual materials
-columns = df.columns.tolist()
-composition_index = columns.index("composition")
-materials_columns = columns[composition_index+1:-1]
+    # Convert material-dataframe to numpy array
+    materials_np = df[materials_columns].to_numpy()
+    price_np = df[['price']].to_numpy()
 
-### Dayun to finish features by eod Tues ###
+    # Add the rest of numerical features of 'price' and each individual materials
+    features = np.concatenate((features, price_np), axis=1)
+    features = np.concatenate((features, materials_np), axis=1)
 
-### end of Dayun's work ###
+    # Save the features as csv file
+    np.savetxt('./data/features.csv', features, delimiter=',')
 
-print(features.shape)
+    return features
 
-# Check how many unique values in each categorical columns
-# print('Unique colors:', len(df["color"].unique()))
-# print('Unique brands:', len(df["brand"].unique()))
-# print('Unique genders:', len(df["gender"].unique()))
 
-#NOTE: use only one of the scaler method below
 
-# # Rescale data
-# scaler = MinMaxScaler(feature_range=(0,1))
-# X_MinMaxScaled = scaler.fit_transform(X)
+if __name__ == "__main__":
 
-# # Standardize Data
-# scaler = StandardScaler().fit(X)
-# X_standardized = scaler.transform(X)
+    # Load dataset
+    df = pd.read_csv("./data/E-Weaver_data.csv", index_col = [0])
 
-# # Normalize Data
-# scaler = Normalizer().fit(X)
-# X_normalized = scaler.transform(X)
+    # Loading features 
+    features = gen_features(df)
 
-# # Summarize transformed data
-# np.set_printoptions(precision=3)
-# print(X_normalized[:5, :])
+    #NOTE: use only one of the scaler method below
+    # # Rescale data
+    # scaler = MinMaxScaler(feature_range=(0,1))
+    # X_MinMaxScaled = scaler.fit_transform(X)
 
-#NOTE: tryout different clustering ml models (interested: K-nearest neighboring models)
-# Define the model
-# model = GaussianMixture(n_components=2)
+    # # Standardize Data
+    # scaler = StandardScaler().fit(X)
+    # X_standardized = scaler.transform(X)
 
-# # Fit the model
-# model.fit(X)
+    # # Normalize Data
+    # scaler = Normalizer().fit(X)
+    # X_normalized = scaler.transform(X)
 
-# # Assign a cluster to each example
-# yhat = model.prediction(X)
+    # # Summarize transformed data
+    # np.set_printoptions(precision=3)
+    # print(X_normalized[:5, :])
 
-# # Retrieve unique clusters
-# clusters = unique(yhat)
+    #NOTE: tryout different clustering ml models (interested: K-nearest neighboring models)
+    # Define the model
+    # model = GaussianMixture(n_components=2)
 
-#NOTE: try visualize our result, the below code might not work, use as reference
-# Create scatter plot for each example from each cluster
-# for cluster in clusters:
-#     # Get each example index for this cluster
-#     row_index = where(yhat == cluster)
-#     # Create scatter plot of these samples after performing PCA
-#     plt.scatter(X[row_index, 0], X[row_index, 1])
-# plt.show()
+    # # Fit the model
+    # model.fit(X)
+
+    # # Assign a cluster to each example
+    # yhat = model.prediction(X)
+
+    # # Retrieve unique clusters
+    # clusters = unique(yhat)
+
+    #NOTE: try visualize our result, the below code might not work, use as reference
+    # Create scatter plot for each example from each cluster
+    # for cluster in clusters:
+    #     # Get each example index for this cluster
+    #     row_index = where(yhat == cluster)
+    #     # Create scatter plot of these samples after performing PCA
+    #     plt.scatter(X[row_index, 0], X[row_index, 1])
+    # plt.show()
